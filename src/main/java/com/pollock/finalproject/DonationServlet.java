65b30983e9f160615e39ed5f7840823e69f6bd8d
Emma Pollock <k0813773@student.kirkwood.edu>
2022-05-01 22:44:12 -0500

    Work on final project

--- src/main/java/com/pollock/finalproject/DonationServlet.java
+++ src/main/java/com/pollock/finalproject/DonationServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -23,7 +24,7 @@ public class DonationServlet extends HttpServlet {
                 donationDetails(request, response);
                 break;
             case "donate":
-                createDonation(request, response);
+                showDonationForm(request, response);
                 break;
             case "my-donations":
 
@@ -40,7 +41,106 @@ public class DonationServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("error", false);
+        try {
+            createDonation(request, response);
+        }
+        catch(Exception ex){
+            request.setAttribute("error", true);
+            request.setAttribute("errorMsg", ex.getMessage());
+        }
+        request.setAttribute("donationSubmitted", true);
+        request.getRequestDispatcher("/WEB-INF/final-project/donate.jsp").forward(request, response);
+    }
 
+    private void createDonation(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try{
+            ArrayList<Person> donors = new ArrayList<>();
+            String givenName1 = request.getParameter("givenName1");
+            if(givenName1 == null || givenName1.equals("")){
+                request.setAttribute("givenName1Error","Please input at least one donor.");
+            }
+            String familyName1 = request.getParameter("familyName1");
+            String email1 = request.getParameter("email1");
+            if(familyName1 == null || familyName1.equals("") || email1 == null || email1.equals("")){
+                request.setAttribute("familyName1Error", "Please finish filling out person 1 information.");
+            }
+
+            String givenName2 = request.getParameter("givenName2");
+            String familyName2 = request.getParameter("familyName2");
+            String email2 = request.getParameter("email2");
+            boolean person2Exists = false;
+            // if the user has filled out any info for person 2
+            if(givenName2 != null && !givenName2.equals("") && familyName2 != null && !familyName2.equals("")
+                    && email2 != null || !email2.equals("")){
+                // if the user hasn't filled out all info for person 2 (email not required)
+                if(givenName2 == null || givenName2.equals("") || familyName1 == null || familyName2.equals("")) {
+                    request.setAttribute("givenName2Error","Please finish filling out person 2 information.");
+                }
+                person2Exists = true;
+            }
+
+            String givenName3 = request.getParameter("givenName3");
+            String familyName3 = request.getParameter("familyName3");
+            String email3 = request.getParameter("email3");
+            boolean person3Exists = false;
+            // if the user has filled out any info for person 3
+            if(givenName3 != null && !givenName3.equals("") && familyName3 != null && !familyName3.equals("")
+                    && email3 != null || !email3.equals("")){
+                // if the user hasn't filled out all info for person 2 (email not required)
+                if(givenName3 == null || givenName3.equals("") || familyName3 == null || familyName3.equals("")) {
+                    request.setAttribute("givenName3Error","Please finish filling out person 3 information.");
+                }
+                person3Exists = true;
+            }
+
+            donors.add(new Person(givenName1, familyName1, email1));
+            if(person2Exists){
+                donors.add(new Person(givenName2, familyName2, email2));
+            }
+            if(person3Exists){
+                donors.add(new Person(givenName3, familyName3, email3));
+            }
+
+            Donation donation = new Donation();
+
+            try{
+                donation.setAmount(request.getParameter("amount"));
+            } catch (Exception ex){
+                request.setAttribute("amountError", ex.getMessage());
+            }
+            try{
+                donation.setFrequency(request.getParameter("frequency"));
+            } catch (Exception ex){
+                request.setAttribute("frequencyError", ex.getMessage());
+            }
+            try{
+                donation.setEnding(request.getParameter("endingDate"));
+            } catch (Exception ex){
+                request.setAttribute("endingDateError", ex.getMessage());
+            }
+            try{
+                donation.setApplyTo(request.getParameter("applyTo"));
+            } catch (Exception ex){
+                request.setAttribute("applyToError", ex.getMessage());
+            }
+            try{
+                donation.setNote(request.getParameter("note"));
+            } catch (Exception ex){
+                request.setAttribute("noteError", ex.getMessage());
+            }
+
+            donation.setPostAmount(request.getParameter("postAmount").equals("true"));
+            donation.setPostName(request.getParameter("postName").equals("true"));
+            donation.setDateTimeProcessed(Instant.now().toString());
+            donation.setDonors(donors);
+
+            donationAccessor.InsertDonation(donation);
+        } catch(Exception ex){
+            throw ex;
+        }
+        request.setAttribute("donationSubmitted", true);
+        request.getRequestDispatcher("/WEB-INF/final-project/donate.jsp").forward(request, response);
     }
 
     private void viewDonations(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -66,7 +166,7 @@ public class DonationServlet extends HttpServlet {
         request.getRequestDispatcher("/WEB-INF/final-project/donationDetails.jsp").forward(request, response);
     }
 
-    private void createDonation(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private void showDonationForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/final-project/donate.jsp").forward(request, response);
     }
 
